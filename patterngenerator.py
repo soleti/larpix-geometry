a4a470cb1a40757843bc75a9755a212b77b399e2
Sam Kohn <kohn@berkeley.edu>
2018-01-06 17:43:09 -0500

    Revise channel assignments to handle grid & triangles

--- patterngenerator.py
+++ patterngenerator.py
@@ -20,73 +20,68 @@ def pixels_plain_grid(nx, ny, dx, dy):
         pixels.append(pixel)
     return pixels
 
-def chips_8x4(chipids, nx, ny):
+grid_4x4_assignments = [1, 2, 0, 3, 5, 6, 4, 7, 8, 11, 9, 10, 12, 15, 13, 14]
+'''
+Index in flattened ``pixelids`` list (from the chip side)::
+
+0  1  2  3
+4  5  6  7
+8  9  10 11
+12 13 14 15
+
+Channel assignments (right side up) (from the chip side)::
+
+2  0  1  3      18 16 17 19
+6  4  5  7  or  22 20 21 23
+8  10 11 9      24 26 27 25
+12 14 15 13     28 30 31 29
+
+'''
+
+triangle_assignments = [0, 1, 3, 4, 2, 5, 6, 7, 8, 9, 10, 13, 11, 12, 14, 15]
+'''
+Index in flattened ``pixelids`` list (from the chip side)::
+
+   0           1
+2     3     4     5
+   6           7
+   8           9
+10    11    12    13
+   14          15
+
+Channel assignments (right-side up) (from the chip side)::
+
+   0           1               16          17
+4     2     3     5         20    18    19    21
+   6           7       or      22          23
+   8           9               24          25
+10    12    13    11        26    28    29    27
+   14          15              30          31
+
+'''
+
+def assign_pixels(pixelids, assignments, right_side_up=True):
     '''
-    Assign the chips sequentially to 8x4 grids of pixels.
+    Return a list assigning the given pixelids to a chip's channels
+    using the provided assignment array.
 
-    Assumes chips are numbered starting at zero, increasing as x
-    increases.
+    The right-side up configuration (``right_side_up=True``) of the chip
+    is (viewed from above), channels 0-15 on the left and channels 16-31
+    on the right. The upside-down configuration is reversed: channels
+    16-31 on the left, and channels 0-15 on the right.
 
-    Pixel IDs:
+    Since the convention is that channel id is given by position in the
+    list, this function always returns assignments to channels 0-15.
+    If the given grid should be assigned to channels 16-31, just
+    concatenate it with the assignments for 0-15.
 
-    0  1  2  3  4  5  6  7
-    8  9  10 11 12 13 14 15
-    16 17 18 19 20 21 22 23
-    24 25 26 27 28 29 30 31
-
-    Channel assignments:
-
-    2  0  1  3  18 16 17 19
-    6  4  5  7  22 20 21 23
-    8  10 11 9  24 26 27 25
-    12 14 15 13 28 30 31 29
+    ``assignments[i]`` gives the index in the final list of
+    ``pixelids[i]``.
 
     '''
-    Dx = 8
-    Dy = 4
-    pixel_ids = np.arange(nx*ny).reshape(ny, nx)
-    print(pixel_ids)
-    assignments = {
-            0: 1,
-            1: 2,
-            2: 0,
-            3: 3,
-            4: 9,
-            5: 10,
-            6: 8,
-            7: 11,
-            8: 16,
-            9: 19,
-            10: 17,
-            11: 18,
-            12: 24,
-            13: 27,
-            14: 25,
-            15: 26,
-            16: 5,
-            17: 6,
-            18: 4,
-            19: 7,
-            20: 13,
-            21: 14,
-            22: 12,
-            23: 15,
-            24: 20,
-            25: 23,
-            26: 21,
-            27: 22,
-            28: 28,
-            29: 31,
-            30: 29,
-            31: 30
-    }
-    chips = []
-    for chipid, (x_start, y_start) in zip(chipids,
-            product(range(0, nx, Dx), range(0, ny, Dx))):
-        subset_pixels = pixel_ids[y_start:y_start+Dy,x_start:x_start+Dx]
-        subset_pixels = subset_pixels.reshape(Dx*Dy)
-        channel_connections = []
-        for channel_id, pixel_id in enumerate(subset_pixels):
-            channel_connections.append(subset_pixels[assignments[channel_id]])
-        chips.append((chipid, channel_connections))
-    return chips
+    if not right_side_up:
+        assignments = assignments[::-1]
+    channel_connections = []
+    for i, pixelid in enumerate(pixelids):
+        channel_connections.append(pixelids[assignments[channel_id]])
+    return channel_connections
