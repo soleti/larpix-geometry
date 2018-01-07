704f35a85ee2916b91305691b4daa7d1f78b51eb
Sam Kohn <kohn@berkeley.edu>
2018-01-06 19:50:24 -0500

    Fix channel assignment patterns

--- patterngenerator.py
+++ patterngenerator.py
@@ -47,7 +47,7 @@ def pixels_triangle_grid(repetition_period, nblocksx, nblocksy, startx,
             pixels.append([pixelid, float(x), float(y), [], []])
     return pixels
 
-grid_4x4_assignments = [1, 2, 0, 3, 5, 6, 4, 7, 8, 11, 9, 10, 12, 15, 13, 14]
+grid_4x4_assignments = [14, 13, 15, 12, 10, 9, 11, 8, 7, 4, 6, 5, 3, 0, 2, 1]
 '''
 Index in flattened ``pixelids`` list (from the chip side)::
 
@@ -58,14 +58,14 @@ Index in flattened ``pixelids`` list (from the chip side)::
 
 Channel assignments (right side up) (from the chip side)::
 
-2  0  1  3      18 16 17 19
-6  4  5  7  or  22 20 21 23
-8  10 11 9      24 26 27 25
-12 14 15 13     28 30 31 29
+13 15 14 12     29 31 30 28
+9  11 10 8  or  25 27 26 24
+7  5  4  6      23 21 20 22
+3  1  0  2      19 17 16 18
 
 '''
 
-triangle_assignments = [0, 1, 3, 4, 2, 5, 6, 7, 8, 9, 10, 13, 11, 12, 14, 15]
+triangle_assignments = [15, 14, 12, 11, 13, 10, 9, 8, 7, 6, 5, 2, 4, 3, 1, 0]
 '''
 Index in flattened ``pixelids`` list (from the chip side)::
 
@@ -78,12 +78,12 @@ Index in flattened ``pixelids`` list (from the chip side)::
 
 Channel assignments (right-side up) (from the chip side)::
 
-   0           1               16          17
-4     2     3     5         20    18    19    21
-   6           7       or      22          23
-   8           9               24          25
-10    12    13    11        26    28    29    27
-   14          15              30          31
+   15          14              31          30
+11    13    12    10        27    29    28    26
+   9           8       or      25          24
+   7           6               23          22
+5     3     2     4         21    19    18    20
+   1           0               17          16
 
 '''
 
