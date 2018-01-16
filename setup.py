062e7db1f860ac4fac2baa0a91e449db28d84199
Sam Kohn <kohn@berkeley.edu>
2018-01-15 16:15:28 -0800

    Add setup.py file for easy installation

---
+++ setup.py
@@ -0,0 +1,17 @@
+'''
+The setup.py file for larpix-geometry.
+
+'''
+
+from setuptools import setup, find_packages
+
+setup(
+        name='larpix-geometry',
+        version='0.1.0',
+        description='LArPix sensor plane geometry',
+        url='https://github.com/samkohn/larpix-geometry',
+        author='Sam Kohn',
+        author_email='skohn@lbl.gov',
+        keywords='dune physics',
+        packages=find_packages(),
+)
