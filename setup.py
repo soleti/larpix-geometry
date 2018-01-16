a384caef341c79511d5bb4f83d7aa73ec2ec8ac7
Sam Kohn <kohn@berkeley.edu>
2018-01-15 17:07:05 -0800

    Bump version number to 0.2.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
         name='larpix-geometry',
-        version='0.1.0',
+        version='0.2.0',
         description='LArPix sensor plane geometry',
         url='https://github.com/samkohn/larpix-geometry',
         author='Sam Kohn',
