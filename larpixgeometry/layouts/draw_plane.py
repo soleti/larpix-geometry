1879fb305a2fb3608756a4dd041c104c49d42461
Sam Kohn <kohn@berkeley.edu>
2018-04-05 15:08:43 -0700

    Improve graphics in draw_plane.py
    - Draw chip id as a watermark behind pixel geometry
    - Improve color scheme
    - Center the number labels (rather than left align)

--- larpixgeometry/layouts/draw_plane.py
+++ larpixgeometry/layouts/draw_plane.py
@@ -11,31 +11,44 @@ import  numpy as np
 with open('sensor_plane_28_8chip.yaml', 'r') as f:
     pixelplane = PixelPlane.fromDict(yaml.load(f))
 
-colors = np.array([[0, 174, 219], [162, 0, 255], [244, 120, 53], [212, 18, 67],
-        [142, 193, 39]])/256.0
-colors = np.vstack((colors, colors))
+colors = np.array([[228, 26, 28], [55, 126, 184], [77, 175, 74], [152,
+    78, 163], [255, 127, 0]])/256.0
+colors = np.tile(colors, (10,1))
 
 
 
 c = canvas.Canvas('sensor_8chip.pdf', pagesize=(12*inch, 12*inch))
 c.setFont('Helvetica', 20)
-c.drawString(4*inch, 11*inch, 'Sensor board with 8 chips')
+c.drawString(4*inch, 11*inch, 'Sensor board with 28 chips')
 c.drawString(4*inch, 10.6*inch, '(view from chip side)')
 c.setFont('Courier', 7)
 colorkey = []
 for pixel in pixelplane.pixels.values():
     c.circle(pixel.x*7-300, 15*inch-pixel.y*7, 0.4)
-    c.drawString(pixel.x*7-300 - 5, 15*inch-pixel.y*7, str(pixel.pixelid))
-c.setFont('Courier', 9)
+    c.drawCentredString(pixel.x*7-300, 15*inch-pixel.y*7, str(pixel.pixelid))
 for chip, color in zip(pixelplane.chips.values(), colors):
-    c.setFillColorRGB(*color)
+    c.setFont('Courier-Bold', 11)
+    c.setFillColorRGB(*color, alpha=1)
     colorkey.append(['Chip %d' % chip.chipid, color])
+    x_sum = 0
+    y_sum = 0
+    count = 0
     for channel, pixel in enumerate(chip.channel_connections):
         if pixel != pixelplane.unconnected_pixel:
             c.drawCentredString(pixel.x*7-300, 15*inch-pixel.y*7-7,
                     str(channel))
-for i, (string, color) in enumerate(colorkey):
-    c.setFillColorRGB(*color)
-    c.drawString(80, 11*inch-25*i, string)
+            x_sum += pixel.x*7-300
+            y_sum += 15*inch-pixel.y*7 - 5
+            count += 1
+    x_avg = x_sum/float(count)
+    y_avg = y_sum/float(count)
+    font = ('Helvetica', 56)
+    c.setFont(*font)
+    c.setFillColorRGB(*color, alpha=0.45)
+    width = c.stringWidth(str(chip.chipid), *font)
+    c.drawCentredString(x_avg, y_avg-16, str(chip.chipid))
+# for i, (string, color) in enumerate(colorkey):
+    # c.setFillColorRGB(*color)
+    # c.drawString(80, 11*inch-25*i, string)
 c.showPage()
 c.save()
