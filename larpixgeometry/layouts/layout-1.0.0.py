3cf3e2f31dfa81407c341d6ace4f380a310ef0f0
Sam Kohn <kohn@berkeley.edu>
2019-09-03 15:18:33 -0700

    Add layout files for original and modified-packaged PCBs
    Original = 4 ASICS, 245, 252, 246, 243
    Modified-packaged = 3 ASICS, 3, 5, 10

---
+++ larpixgeometry/layouts/layout-1.0.0.py
@@ -0,0 +1,36 @@
+'''
+Generate a YAML file with the pixel layout and chip connections for the
+original 4-chip sensor plane, with no focusing grids or extended pads.
+
+'''
+import yaml
+import patterngenerator as pg
+
+pixels = []
+subgrid_width = 12
+pixels.extend(pg.pixels_triangle_grid(subgrid_width, 4, 1, 76, 88,
+    len(pixels)))
+pixels.extend(pg.pixels_plain_grid(3, 4, 1, 77.5, 113.5, len(pixels)))
+
+pixelids = {
+        # Bool value is argument to right_side_up
+        # ranges are pixel id ranges for 0-15, 16-31
+        245: (False, 'triangle', range(1*16, 2*16), range(0*16, 1*16)),
+        252: (True, 'triangle', range(2*16, 3*16), range(3*16, 4*16)),
+        246: (False, 'plain', range(5*16, 6*16), range(4*16, 5*16)),
+        243: (True, 'plain', range(6*16, 7*16), range(7*16, 8*16))
+}
+
+chips = []
+for chipid, (right_side_up, shape, first_ids, second_ids) in pixelids.items():
+    if shape == 'plain':
+        assignment = pg.grid_4x4_assignments_v1
+    else:
+        assignment = pg.triangle_assignments_v1
+    first_channels = pg.assign_pixels(first_ids, assignment, right_side_up)
+    second_channels = pg.assign_pixels(second_ids, assignment, right_side_up)
+    chips.append([chipid, first_channels + second_channels])
+
+
+with open('layout-1.0.0.yaml', 'w') as f:
+    yaml.dump({'pixels': pixels, 'chips': chips}, f)
---
+++ larpixgeometry/layouts/layout-1.2.1.py
@@ -0,0 +1,34 @@
+'''
+Generate a YAML file with the pixel layout and chip connections for the
+packaged 4-chip sensor plane, (first 3 chips only), with no focusing
+grids or extended pads.
+
+'''
+import yaml
+import patterngenerator as pg
+
+pixels = []
+subgrid_width = 16
+pixels.extend(pg.pixels_plain_grid(4, 2, 1, 53.5, 65.5, len(pixels)))
+pixels.extend(pg.pixels_plain_grid(4, 2, 1, 97.5, 65.5, len(pixels)))
+pixels.extend(pg.pixels_plain_grid(4, 2, 1, 53.5, 109.5, len(pixels)))
+
+pixelids = {
+        # Bool value is argument to right_side_up
+        # ranges are pixel id ranges for 0-15, 16-31
+        3: (True, 'plain', range(0*16, 1*16), range(1*16, 2*16)),
+        5: (True, 'plain', range(2*16, 3*16), range(3*16, 4*16)),
+        10: (True, 'plain', range(4*16, 5*16), range(5*16, 6*16)),
+}
+
+chips = []
+for chipid, (right_side_up, shape, first_ids, second_ids) in pixelids.items():
+    assignment_1 = pg.grid_4x4_assignments_0_15_v1_2
+    assignment_2 = pg.grid_4x4_assignments_16_31_v1_2
+    first_channels = pg.assign_pixels(first_ids, assignment_1, right_side_up)
+    second_channels = pg.assign_pixels(second_ids, assignment_2, right_side_up)
+    chips.append([chipid, first_channels + second_channels])
+
+
+with open('layout-1.2.1.yaml', 'w') as f:
+    yaml.dump({'pixels': pixels, 'chips': chips}, f)
