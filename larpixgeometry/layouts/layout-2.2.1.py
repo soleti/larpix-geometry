6fd26e0359403c93ea299a8b1eb17d9a80afe05d
Peter Madigan <pmadigan@berkeley.edu>
2020-07-28 11:03:45 -0700

    Fix chip y pos in layout-2.2.1

--- larpixgeometry/layouts/layout-2.2.1.py
+++ larpixgeometry/layouts/layout-2.2.1.py
@@ -14,7 +14,7 @@ width = 7*pixel_pitch*3
 height = 7*pixel_pitch*3
 for chip in chip_ids:
     x = (chip // 10 - 1) * 7 * pixel_pitch + pixel_pitch/2 - width/2
-    y = (chip % 10 - 2) * 7 * pixel_pitch + pixel_pitch/2 - height/2
+    y = (4 - chip % 10) * 7 * pixel_pitch + pixel_pitch/2 - height/2
     pixels.extend(pg.pixels_plain_grid(pixel_pitch, 1, 1, x, y, len(pixels), batch_size=7, pixels_per_grid=49))
 
 pixelids = dict()
