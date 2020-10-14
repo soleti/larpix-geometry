8b583e646e739ec8f34539f13657e50535670076
BERM <brookerussellphysics@gmail.com>
2020-10-14 14:00:25 -0700

    10x10 tile

--- larpixgeometry/layouts/layout-2.4.0.py
+++ larpixgeometry/layouts/layout-2.4.0.py
@@ -1,6 +1,6 @@
 '''
 Generate a YAML file with the pixel layout and chip connections for the
-packaged 3x3-rev2 pixel tile
+packaged 10x10 pixel tile
 
 '''
 import yaml
@@ -8,17 +8,8 @@ import patterngenerator as pg
 
 pixels = []
 subgrid_width = 16
-chip_ids = list(range(11,20)) \
-    + list(range(21,30)) \
-    + list(range(31,40)) \
-    + list(range(41,50)) \
-    + list(range(51,60)) \
-    + list(range(61,70)) \
-    + list(range(71,80)) \
-    + list(range(81,90)) \
-    + list(range(91,100)) \
-    + list(range(101,110)) \
-    + list(range(110,201,10))
+chip_ids = list(range(11,111))
+
 print(chip_ids)
 print('chips',len(chip_ids))
 pixel_pitch = 4.434
@@ -26,18 +17,20 @@ width = 7*pixel_pitch*10
 height = 7*pixel_pitch*10
 
 two_digit_xy = lambda x: ((x%10-1), (x//10-1))
-last_column_xy = lambda x: (9, ((x-100)//10-1))
-last_row_xy = lambda x: (((x-100)%10-1), 9)
+last_column_xy = lambda x: (9, (x//10-2))
+last_row_xy = lambda x: ((x%100-1), 9)
+
+print(len(chip_ids),'chip ids')
 
 for chip in chip_ids:
     if chip < 100:
         x,y = two_digit_xy(chip)
-    elif chip < 110:
+    elif chip>=100:
         x,y = last_row_xy(chip)
-    elif chip < 200:
+
+    if chip%10==0:
         x,y = last_column_xy(chip)
-    else:
-        x,y = (9,9)
+
     x = x * 7 * pixel_pitch + pixel_pitch/2 - width/2
     y = y * 7 * pixel_pitch + pixel_pitch/2 - height/2
     pixels.extend(pg.pixels_plain_grid(pixel_pitch, 1, 1, x, y, len(pixels), batch_size=7, pixels_per_grid=49))
@@ -55,6 +48,6 @@ for chipid, (right_side_up, shape, ids) in pixelids.items():
     chips.append([chipid, channels])
 print('chips',len(chips))
 
-with open('layout-2.3.0.yaml', 'w') as f:
+with open('layout-2.4.0.yaml', 'w') as f:
     yaml.dump({'pixels': pixels, 'chips': chips, 'x': -width/2, 'y': -height/2,
         'width': width, 'height': height}, f)
