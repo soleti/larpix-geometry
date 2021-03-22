d0316ec85039a1be5d5ea35bee34ba5c8b3b84ed
soleti <roberto@lbl.gov>
2021-03-22 16:58:09 -0700

    Added orientation for each tile

--- larpixgeometry/layouts/multi_tile_layout.py
+++ larpixgeometry/layouts/multi_tile_layout.py
@@ -13,12 +13,11 @@ a multi-tile LArPix anode
     and the value is a pair of vectors: position and orientation
     of the tile
 - tile_chip_to_io: nested dictionary where the first key is tile ID
-    and the second key is chip ID and the value is IO channel +
-    1000 * IO group. Tile ID, chip ID, IO channel, and IO group are
-    all type integers
-- chip_channel_to_position: dictionary where the key is channel ID +
-    1000 * chip ID and the value is (x-position, y-position),
-    stored as multiples of the pixel pitch of type integer
+    and the second key is chip ID and the value is (IO channel, IO group).
+    Tile ID, chip ID, IO channel, and IO group are all type integers
+- chip_channel_to_position: dictionary where the key is (channel ID, chip ID)
+  and the value is (x-position, y-position), stored as multiples of the pixel pitch
+  of type integer
 """
 
 import json
@@ -79,22 +78,56 @@ def generate_layout(tile_layout_file, network_config_file, n_tiles, pixel_pitch=
     ## The numbers are in mm and were provided by Patrick Koller.
     ## The anode is on the yz plane with the pixels oriented
     ## towards the positive x axis
-    tile_positions = {1: [[-315.1745,465.2,-155.2],[1,0,0]],
-                      2: [[-315.1745,465.2,155.2],[1,0,0]],
-                      3: [[-315.1745,155.2,-155.2],[1,0,0]],
-                      4: [[-315.1745,155.2,155.2],[1,0,0]],
-                      5: [[-315.1745,-155.2,-155.2],[1,0,0]],
-                      6: [[-315.1745,-155.2,155.2],[1,0,0]],
-                      7: [[-315.1745,-465.2,-155.2],[1,0,0]],
-                      8: [[-315.1745,-465.2,155.2],[1,0,0]],
-                      9: [[-315.1745,465.2,-155.2],[-1,0,0]],
-                      10: [[315.1745,465.2,155.2],[-1,0,0]],
-                      11: [[315.1745,155.2,-155.2],[-1,0,0]],
-                      12: [[315.1745,155.2,155.2],[-1,0,0]],
-                      13: [[315.1745,-155.2,-155.2],[-1,0,0]],
-                      14: [[315.1745,-155.2,155.2],[-1,0,0]],
-                      15: [[315.1745,-465.2,-155.2],[-1,0,0]],
-                      16: [[315.1745,-465.2,155.2],[-1,0,0]]}
+    tile_indeces = {1:  [1,1,1],
+                    2:  [1,1,2],
+                    3:  [1,1,3],
+                    4:  [1,1,4],
+                    5:  [1,1,5],
+                    6:  [1,1,6],
+                    7:  [1,1,7],
+                    8:  [1,1,8],
+                    9:  [1,2,1],
+                    10: [1,2,2],
+                    11: [1,2,3],
+                    12: [1,2,4],
+                    13: [1,2,5],
+                    14: [1,2,6],
+                    15: [1,2,7],
+                    16: [1,2,8]}
+
+    tile_positions = {1:  [-315.1745, 465.2,-155.2],
+                      2:  [-315.1745, 465.2, 155.2],
+                      3:  [-315.1745, 155.2,-155.2],
+                      4:  [-315.1745, 155.2, 155.2],
+                      5:  [-315.1745,-155.2,-155.2],
+                      6:  [-315.1745,-155.2, 155.2],
+                      7:  [-315.1745,-465.2,-155.2],
+                      8:  [-315.1745,-465.2, 155.2],
+                      9:  [ 315.1745, 465.2,-155.2],
+                      10: [ 315.1745, 465.2, 155.2],
+                      11: [ 315.1745, 155.2,-155.2],
+                      12: [ 315.1745, 155.2, 155.2],
+                      13: [ 315.1745,-155.2,-155.2],
+                      14: [ 315.1745,-155.2, 155.2],
+                      15: [ 315.1745,-465.2,-155.2],
+                      16: [ 315.1745,-465.2, 155.2]}
+
+    tile_orientations = {1:  [ 1,-1, 1],
+                         2:  [ 1, 1,-1],
+                         3:  [ 1,-1, 1],
+                         4:  [ 1, 1,-1],
+                         5:  [ 1,-1, 1],
+                         6:  [ 1, 1,-1],
+                         7:  [ 1,-1, 1],
+                         8:  [ 1, 1,-1],
+                         9:  [-1,-1, 1],
+                         10: [-1, 1,-1],
+                         11: [-1,-1, 1],
+                         12: [-1, 1,-1],
+                         13: [-1,-1, 1],
+                         14: [-1, 1,-1],
+                         15: [-1,-1, 1],
+                         16: [-1, 1,-1]}
 
     tile_chip_io_channel_io_group = {it:{} for it in range(1,n_tiles+1)}
 
@@ -102,22 +135,33 @@ def generate_layout(tile_layout_file, network_config_file, n_tiles, pixel_pitch=
         io_channels = io_channels_tile[tile_id]
         for io in io_channels:
             for chip in io_channels[io]:
-                tile_chip_io_channel_io_group[tile_id][chip] = tile_id*1000+io
+                tile_chip_io_channel_io_group[tile_id][chip] = tile_id*1000 + io
 
     chip_channel = {}
+
+    xs = []
+    ys = []
+    for chip in chipids:
+        for channel, pixel in enumerate(board.chips[chip].channel_connections):
+            if pixel.x !=0 and pixel.y != 0:
+                xs.append(pixel.x)
+                ys.append(pixel.y)
 
     for chip in chipids:
         for channel, pixel in enumerate(board.chips[chip].channel_connections):
             if pixel.x !=0 and pixel.y != 0:
-                key = chip*1000 + channel
-                chip_channel[key] = [round(pixel.x / pixel_pitch), round(pixel.y / pixel_pitch)]
+                key = chip*1000+channel
+                chip_channel[key] = [round((pixel.x - min(xs))/pixel_pitch),
+                                     round((pixel.y - min(ys))/pixel_pitch)]
 
-    with open('multi_tile_layout-%s2.yaml' % FORMAT_VERSION, 'w') as f:
+    with open('multi_tile_layout-%s.yaml' % FORMAT_VERSION, 'w') as f:
         yaml.dump({'tile_layout_version': LAYOUT_VERSION,
                    'multitile_layout_version': FORMAT_VERSION,
                    'pixel_pitch': pixel_pitch,
                    'tile_positions': tile_positions,
+                   'tile_orientations': tile_orientations,
                    'tile_chip_to_io': tile_chip_io_channel_io_group,
+                   'tile_indeces': tile_indeces,
                    'chip_channel_to_position': chip_channel}, f)
 
 if __name__ == "__main__":
